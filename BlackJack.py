bb12eaf038672f6841339dbb452d5a62fd6b54a8
tylerpiercey <122289319+tylerpiercey@users.noreply.github.com>
2023-04-08 16:05:29 -0230

    handling aces and formatting- alot of troubleshooting

--- BlackJack.py
+++ BlackJack.py
@@ -1,93 +1,112 @@
 import random
 
-# create a list to store suit rank and point value
-#
+def title():
+    print("BLACKJACK!")
+    print("Blackjack payout is 3:2")
+    print()
 
-suits = ["Hearts", "Diamonds", "Clubs", "Spades"]
-ranks = ["Ace", "King", "Queen", "Jack", "10", "9", "8", "7", "6", "5", "4", "3", "2"]
-pointValues = [11, 10, 10, 10, 10, 9, 8, 7, 6, 5, 4, 3, 2]
+def makeDeck():
+    suits = ["Hearts", "Diamonds", "Clubs", "Spades"]
+    ranks = ["Ace", "King", "Queen", "Jack", "10", "9", "8", "7", "6", "5", "4", "3", "2"]
+    pointValues = [11, 10, 10, 10, 10, 9, 8, 7, 6, 5, 4, 3, 2]
 
-deck = []
+    deck = []
 
-for suit in suits:
-    for rank in ranks:
-        deck.append([rank, suit, pointValues[ranks.index(rank)]])
+    for suit in suits:
+        for rank in ranks:
+            deck.append([rank, suit, pointValues[ranks.index(rank)]])
+    return deck
 
-while True:
-    playerHand = []
-    dealerHand = []
-
-    random.shuffle(deck)
-
-    playerHand.append(deck.pop())
-    playerHand.append(deck.pop())
-
-
+def stand(dealerHand, playerTotal, dealerTotal, deck):
     dealerHand.append(deck.pop())
-
-
-    print("DEALER'S SHOW CARD:")
-    print(f"{dealerHand[0][0]} of {playerHand[0][1]}")
-    print()
-    print("YOUR CARDS:")
-    print(F"{playerHand[0][0]} of {playerHand[0][1]}")
-    print(F"{playerHand[1][0]} of {playerHand[1][1]}")
-    playerTotal = playerHand[0][2] + playerHand[1][2]
-    dealerTotal = dealerHand[0][2]
-
-
-    while playerTotal < 22:
-        choice = input("Hit or Stand (hit/stand): ")
-        print()
-        if choice.lower() == "s":
+    dealerTotal += dealerHand[1][2]
+    print("DEALER' CARDS:")
+    for i in range(len(dealerHand)):
+        while dealerTotal < 17:
             dealerHand.append(deck.pop())
-            dealerTotal += dealerHand[1][2]
-            #if dealerTotal == 21 and playerTotal == 21:
-            print("DEALER' CARDS:")
-            for i in range(len(dealerHand)):
-                while dealerTotal < 17:
-                    dealerHand.append(deck.pop())
-                    dealerTotal += dealerHand[i+2][2]
-            for i in range(len(dealerHand)):
-                print(F"{dealerHand[i][0]} of {dealerHand[i][1]}")
-            if dealerTotal > 21 or dealerTotal < playerTotal:
-                print()
-                print("Player wins!")
-                print()
-            elif playerTotal == dealerTotal:
-                print("Draw!")
-            else:
-                print("Sorry. You lose.")
-                print()
-            print(f"YOUR POINTS: {playerTotal}")
-            print(f"DEALER'S POINTS: {dealerTotal}")
-            print()
-            break
-        elif choice.lower() == "h":
-            playerTotal = 0
-            playerHand.append(deck.pop())
-            print("YOUR CARDS:")
-            for i in range(len(playerHand)):
-                print(F"{playerHand[i][0]} of {playerHand[i][1]}")
-                playerTotal += playerHand[i][2]
-            print(playerTotal)
-            if playerTotal > 21:
-                print("sorry. you lose.")
-                print(playerTotal)
-        else:
-            print("Invalid Input")
-    playAgain = input("Play again (y/n): ")
-    print()
-    if playAgain.lower() == "n":
-        print("Come back soon!")
-        print("Bye!")
-        break
-    elif playAgain.lower() == "y":
-        continue
+            dealerTotal += dealerHand[i + 2][2]
+    for i in range(len(dealerHand)):
+        print(F"{dealerHand[i][0]} of {dealerHand[i][1]}")
+    if dealerTotal > 21 or dealerTotal < playerTotal:
+        print()
+        print(f"YOUR POINTS: {playerTotal}")
+        print(f"DEALER'S POINTS: {dealerTotal}")
+        print()
+        print("Player wins!")
+        print()
+    elif playerTotal == dealerTotal:
+        print("Draw!")
     else:
-        print("invalid input. Closing program")
-        break
+        print()
+        print(f"YOUR POINTS: {playerTotal}")
+        print(f"DEALER'S POINTS: {dealerTotal}")
+        print()
+        print("Sorry. You lose.")
+
+def main():
+    deck = makeDeck()
+    title()
+    while True:
+
+        playerHand = []
+        dealerHand = []
+
+        random.shuffle(deck)
+
+        playerHand.append(deck.pop())
+        playerHand.append(deck.pop())
+
+        dealerHand.append(deck.pop())
+
+        print("DEALER'S SHOW CARD:")
+        print(f"{dealerHand[0][0]} of {playerHand[0][1]}")
+        print()
+        print("YOUR CARDS:")
+        print(F"{playerHand[0][0]} of {playerHand[0][1]}")
+        print(F"{playerHand[1][0]} of {playerHand[1][1]}")
+        print()
+        playerTotal = playerHand[0][2] + playerHand[1][2]
+        dealerTotal = dealerHand[0][2]
+
+        while playerTotal < 23:
+            choice = input("Hit or Stand (hit/stand): ")
+            print()
+            if choice.lower() == "s":
+                stand(dealerHand, playerTotal, dealerTotal, deck)
+                break
+            elif choice.lower() == "h":
+                aces = 0
+                playerTotal = 0
+                playerHand.append(deck.pop())
+                print("YOUR CARDS:")
+                for i in range(len(playerHand)):
+                    print(F"{playerHand[i][0]} of {playerHand[i][1]}")
+                    if playerHand[i][0] == "Ace":
+                        aces += 1
+                    playerTotal += playerHand[i][2]
+                    if playerTotal > 21 and aces > 0:
+                        playerTotal -= 10
+                        aces -= 1
+                print()
+                print(f"Current score: {playerTotal}")
+                print()
+                if playerTotal > 21:
+                    print("sorry. you lose.")
+                    print()
+            else:
+                print("Invalid Input")
+        playAgain = input("Play again (y/n): ")
+        print()
+        if playAgain.lower() == "n":
+            print("Come back soon!")
+            print("Bye!")
+            break
+        elif playAgain.lower() == "y":
+            continue
+        else:
+            print("invalid input. Closing program")
+            break
 
 
-
-
+if __name__ == "__main__":
+    main()
